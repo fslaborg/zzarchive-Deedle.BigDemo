f38a256fabfc6bdae4aeb76673fe1676c52dbfc9
Tomas Petricek <tomas@tomasp.net>
2015-07-09 15:15:48 +0100

    Reset and remove connection keys

--- utils/credentials.fsx
+++ utils/credentials.fsx
@@ -1,9 +1,9 @@
 #load "mbrace.fsx"
 
 [<Literal>]
-let storageConnection = "DefaultEndpointsProtocol=https;AccountName=bigdeedle;AccountKey=YRqnnp5+dnDtJTthmkdkbntkeEEcTSVvzmZiO7brVsPUb3FFg3vDnt15UHazFPVSHNY7zZS5TvFyPg3fXE7dfQ==" // "<Insert your storage connection>"
+let storageConnection = "<Insert your storage connection>"
 [<Literal>]
-let serviceBusConnection = "Endpoint=sb://brisk-eus30565217be8e.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=WxyJ0yQeF56cZd/RNIKWK/QqW9huwYzlln3kj+T+Hk8=" // "<Insert your service bus connection>"
+let serviceBusConnection = "<Insert your service bus connection>"
 
 let config =
     { MBrace.Azure.Configuration.Default with
